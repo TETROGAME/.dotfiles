a2ba51317b81cf7a8372398e145cfcb4a18b59e7
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-08-15 22:43:28 +0300

    (nvim) add niri blur support

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -3,9 +3,26 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- 1. Сначала загружаем саму тему
+		-- 1. Load the colorscheme
 		vim.cmd([[colorscheme thorn]])
 
+		-- 2. Clear background highlights to enable Niri's compositor blur
+		local groups = {
+			"Normal",
+			"NormalNC",
+			"NormalFloat",
+			"FloatBorder",
+			"SignColumn",
+			"EndOfBuffer",
+			"NeoTreeNormal",
+			"NeoTreeNormalNC",
+		}
+
+		for _, group in ipairs(groups) do
+			vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+		end
+
+		-- 3. Apply your custom highlights
 		vim.api.nvim_set_hl(0, "LineNr", { fg = "#9ec59b" })
 		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#9ec59b", bold = true })
 		vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "#ff5555" })
